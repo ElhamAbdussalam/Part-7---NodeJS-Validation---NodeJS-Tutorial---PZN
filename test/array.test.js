9a9e2e4b211ce8c7eec5790176fb3bbd21b11ab8
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-11 18:49:55 +0700

    array

--- test/array.test.js
+++ test/array.test.js
@@ -9,4 +9,22 @@ describe("Array", () => {
     const result = hobbiesSchema.validate(hobbies);
     console.info(result);
   });
+
+  it("should can validate array of object", () => {
+    const addressSchema = Joi.array()
+      .min(1)
+      .items(
+        Joi.object({
+          street: Joi.string().required().max(200),
+          city: Joi.string().required().max(100),
+          country: Joi.string().required().max(100),
+        }),
+      );
+
+    const addresses = [];
+    const result = addressSchema.validate(addresses, {
+      abortEarly: false,
+    });
+    console.info(result);
+  });
 });
