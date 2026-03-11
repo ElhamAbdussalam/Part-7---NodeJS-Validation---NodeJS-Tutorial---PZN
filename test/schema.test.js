d9385645b53c236f3effe30ac72ba3caee4fb110
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-11 12:46:42 +0700

    schema

--- test/schema.test.js
+++ test/schema.test.js
@@ -38,4 +38,17 @@ describe("Joi", () => {
     console.info(result2);
     console.info(result2.error);
   });
+
+  it("should return validation error", () => {
+    const usernameSchema = Joi.string().min(5).email().required();
+
+    const result = usernameSchema.validate("ups");
+    console.info(result.value);
+
+    if (result.error) {
+      result.error.details.forEach((detail) => {
+        console.info(`${detail.path} = ${detail.message}`);
+      });
+    }
+  });
 });
